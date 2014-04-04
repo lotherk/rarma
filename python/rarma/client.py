9686c62eddb106f18f7d4932d849e62179d392d9
Konrad Lother <konrad@corpex.de>
2014-04-05 00:33:40 +0200

    added rhash method to convert json into rarma hash. partial working

--- python/rarma/client.py
+++ python/rarma/client.py
@@ -1,6 +1,7 @@
 import socket
 import json
-
+import numpy
+import re
 
 class Client():
     def __init__(self):
@@ -14,7 +15,23 @@ class Client():
     def close(self):
         self.sock.close()
 
-    def send(self, tid, message):
+    def send(self, message):
+        jdic = {}
+        try:
+            result = eval(message) # whaaaaat??? yes.
+            for kv in result:
+                jdic.update({kv[0]:kv[1]})
+        except:
+            return '[["error","malformed message. must be hash"]]'
         bmsg = bytes(message)
-        self.sock.send(bmsg)
-        return str(self.sock.recv(1024))
+        self.sock.send(json.dumps(jdic) + "\n")
+        ret = self.sock.recv(1024)
+        print eval(str(self.rhash(json.loads(ret))))
+
+    def rhash(self, json):
+        ret = []
+        for k,v in json.iteritems():
+            if type(v) == dict:
+                v = self.rhash(v)
+            ret.append([str(k),v])
+        return str(ret)
