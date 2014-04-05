6087dd908d0336f3aec32382ac523529bfe8c557
Konrad Lother <konrad@corpex.de>
2014-04-05 08:41:38 +0200

    new macro

--- sqf/rarma/include/rarma.h
+++ sqf/rarma/include/rarma.h
@@ -4,3 +4,4 @@
 #define RDEFAULT_PARAM(idx,dft) if(!isNil "_this") then {if(typeName _this == 'ARRAY') then {if(count _this < idx) then {_this select idx} else {dflt}} else {_this}} else {dflt};
 #define RPARAM(dft) if(!isNil "_this") then {_this} else {def}
 #define RPARAMS(idx, dft) RDEFAULT_PARAM(idx, dft)
+#define RCVAR(var) MEMBER("@"+var,nil)
