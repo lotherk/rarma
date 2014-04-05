b41fa31d84d69603fddeda15dbfbe8bdd7e98634
Konrad Lother <konrad@corpex.de>
2014-04-05 19:01:25 +0200

    working.

--- sqf/rarma/client/client.rb
+++ sqf/rarma/client/client.rb
@@ -7,7 +7,6 @@ class Rarma::Client
     <<-SQF
     _str = ("Py from rarma.client import Client");
     ("Arma2Net.Unmanaged" callExtension _str);
-    
     if(isNil "rarma_client_ref_index") then { rarma_client_ref_index = 0 };
     _varname = format["client_ref_%1", rarma_client_ref_index];
     _str = format["PY %1 = Client(); print %1;", _varname];
@@ -31,19 +30,23 @@ class Rarma::Client
     _ref = MEMBER("@ref",nil);
     _tid = ("Arma2Net.Unmanaged" callExtension format["Py %1.get_tid()", _ref]);
     // this is.. ugly
+
+    if(typeName _message != 'string') then {
+      _message = str(_message);
+    };
+
     _split = [_message, ""] call CBA_fnc_split;
     _length = count _split;
     _index = 0;
     _size = 448;
     _packet = [];
     _result = nil;
-
     {
       _packet = _packet + [_x];
 
-      if(_index == _size or _index == _length) then {
+      if(_index == _size or _index == (_length - 1)) then {
         _msg = [_packet, ""] call CBA_fnc_join;
-        _msg = [_msg, '"', '\"'] call CBA_fnc_replace;
+        _msg = [_msg, '"', '\\"'] call CBA_fnc_replace;
         _str = format["py %1.q_append(%2, %3)", MEMBER("@ref",nil), _tid, str(_msg)];
         _res = ("Arma2Net.Unmanaged" callExtension _str);
         _index = 0;
