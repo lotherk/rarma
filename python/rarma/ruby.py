45a26be5c6c2d555b9e9d9cbb10f6e6df6a3b2a8
Konrad Lother <konrad@corpex.de>
2014-04-08 23:47:19 +0200

    might be simpler

--- python/rarma/ruby.py
+++ python/rarma/ruby.py
@@ -2,13 +2,8 @@ import clr
 clr.AddReference('IronRuby') # ensure that IronRuby is installed.
 from IronPython import Ruby
 
-class RubyInstance():
-    def __init__(self,scope=None):
-        self.ruby_engine = Ruby.CreateEngine():
-        if not scope:
-            self.ruby_scope = self.ruby_engine.CreateScope()
-        else:
-            self.ruby_scope = scope
+ruby_engine = Ruby.CreateEngine()
+ruby_scope = self.ruby_engine.CreateScope()
 
-    def execute(self,script):
-        self.ruby_engine.Execute(script, self.ruby_scope)
+def ruby(script)
+    return ruby_engine.Execute(script,scope)
