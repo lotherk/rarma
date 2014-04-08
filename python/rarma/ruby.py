71482cff9e77d3430d6330906860940dcc705205
Konrad Lother <konrad@corpex.de>
2014-04-08 23:53:32 +0200

    python..

--- python/rarma/ruby.py
+++ python/rarma/ruby.py
@@ -5,5 +5,5 @@ from IronPython import Ruby
 ruby_engine = Ruby.CreateEngine()
 ruby_scope = self.ruby_engine.CreateScope()
 
-def ruby(script)
+def ruby(script):
     return ruby_engine.Execute(script,scope)
