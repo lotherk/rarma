4a036554abccbac01add589b657190afe291dbd7
Lukas Kropatschek <lukas@kropatschek.net>
2014-04-10 17:37:54 +0200

    fixed logger

--- sqf/rarma/logger.rb
+++ sqf/rarma/logger.rb
@@ -4,7 +4,7 @@ module Rarma::Logger
     <<-SQF
     // strip mission path from _file
 
-    _format = format["%1|%2|%3 %4:%5 %6", time, diag_frameno, diag_fps, _lineno, _file, _message];
+    _format = format["%1|%2|%3 %4:%5 %6", time, diag_frameno, diag_fps, _file, _lineno, _message];
     diag_log _format;
     SQF
   end
