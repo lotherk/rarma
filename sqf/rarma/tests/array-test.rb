fc9086f6ed00cd469abd417d5740a9c787b3ebb7
Lukas Kropatschek <lukas@kropatschek.net>
2014-04-10 18:13:31 +0200

    array test case (not implemented) :P

---
+++ sqf/rarma/tests/array-test.rb
@@ -0,0 +1,17 @@
+class ArrayTest
+  # prepare test-case
+  __native
+  def initialize
+    <<-SQF
+
+    SQF
+  end
+
+  __native
+  # start the tes-case
+  def start
+    <<-SQF
+
+    SQF
+  end
+end
