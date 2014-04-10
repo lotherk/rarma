7d439cad723a08eceddbff0feceef3fe54e57f35
Konrad Lother <konrad@corpex.de>
2014-04-10 19:18:26 +0200

    changed method self.initialize to self.init for beauty reasons

--- sqf/rarma/kernel.rb
+++ sqf/rarma/kernel.rb
@@ -5,17 +5,16 @@
 
 module Rarma::Kernel
   __native
-  def self.initialize
+  def self.init
     <<-SQF
     _succ = ("Arma2Net.Unmanaged" callExtension "PY import rarma");
-    if(_succ == "") then { 
+    if(_succ == "") then {
         LOGGER("Loaded Ruby from Python successfully")
-    } else { 
+    } else {
         LOGGER_FATAL("Couldn't load Ruby from Python."); 
         LOGGER_FATAL("Output:\n" + _succ);
         LOGGER_FATAL("End of output.");
     };
-    
     SQF
   end
 end
