b11ee2d4e75b679a63be1e163528b0b62addcd21
Konrad Lother <konrad@corpex.de>
2014-04-12 00:21:55 +0200

    has to be tested!

--- lib/rarma/sqf/compiler/processor/op_asgn_or.rb
+++ lib/rarma/sqf/compiler/processor/op_asgn_or.rb
@@ -1,8 +1,14 @@
 module Rarma::SQF::Compiler::Processor::Op_asgn_or
   def process_op_asgn_or exp
-   a = self.class.new
-   a.process exp
-   raise a.script.join("\n")
-   exp
+    a = self.class.new
+    a.process exp.shift
+    left = a.script.join("\n")
+    a = self.class.new
+    a.process exp.shift
+    right = a.script.join("\n")
+    @script << <<-SQF
+      if(isNil #{left}) then { #{right} };
+    SQF
+    exp
   end
 end
