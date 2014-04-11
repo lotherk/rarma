071d8815851096698a8e6a7cdc25dcaa10cd2955
Lukas Kropatschek <lukas@kropatschek.net>
2014-04-11 22:59:59 +0200

    additions

--- sqf/rarma/lib/core/sqfobject.rb
+++ sqf/rarma/lib/core/sqfobject.rb
@@ -1,17 +1,29 @@
 class Rarma::SQFObject
-  attr_accessor :posATL, :posASL, :dir, :damage, :alive, :classname, :simulation
-  attr_reader :variables, :netid
+  attr_accessor :posATL, :posASL, :dir, :damage, :alive, :simulation, :name,
+    :event_handlers
+  attr_reader :variables, :netid, :classname, :side
+  
 
-  def initialize _classname=nil
-    @classname = _classname
+  def initialize classname=nil
+    @classname = classname
     @posASL = [0,0,0]
     @posATL = [0,0,0]
     @dir = 0
     @damage = 0
     @alive = false
     @variables = []
-    @netid = nil
     @simulation = false
+    @event_handlers = []
+  end
+
+  def add_variable _key, _value
+    @variables << [_key, _value]
+    setVariable @this, _key, _value
+  end
+
+  def add_event_handler _type, _code
+    @event_handlers << [_type, _code]
+    addEventHandler @this, _type, _code
   end
 
 end
--- sqf/rarma/lib/core/unit.rb
+++ sqf/rarma/lib/core/unit.rb
@@ -1,32 +1,35 @@
 class Rarma::Unit < Rarma::SQFObject
-  attr_accessor :group, :event_handlers, :score, :rating, :rank, :weapons, :magazines, 
-    :side, :skill, :subskills, :unitpos, :uniform, :vest, :headgear, :items, 
-    :waypoints, :name, :formation_pos, :linked_items, :primary_weapon, :secondary_weapon,
-    :primary_weapon_items, :secondary_weapon_items
+  attr_accessor :group, :score, :rating, :rank, :weapons, :magazines, 
+    :skill, :subskills, :unitpos, :uniform, :vest, :headgear, :items, 
+    :waypoints, :formation_pos, :linked_items, :primary_weapon, :secondary_weapon,
+    :handgun, :primary_weapon_items, :secondary_weapon_items, :handgun_items
 
   def initialize
-    @group = nil
-    @side = nil
-    @event_handlers = []
     @score = 0
-    @rating = nil
-    @rank = nil
     @weapons = []
     @magazines = []
-    @uniform = nil
-    @vest = nil
-    @headgear = nil
     @items = []
     @waypoints = []
-    @unitpos = nil
-    @name = nil
-    @skill = nil
     @subskills = []
-    @formation_pos = nil
     @linked_items = []
-    @primary_weapon = nil
-    @secondayr_weapon = nil
     @primary_weapon_items = []
     @secondary_weapon_items = []
+    @handgun_items = []
   end
+
+  def add_primary_weapon_item _item
+    @primary_weapon_items << _item
+    addPrimaryWeaponItem @this, _item
+  end
+
+  def add_secondary_weapon_item _item
+    @secondary_weapon_items << _item
+    addSecondaryWeaponItem @this, _item
+  end
+
+  def add_handgun_item _item
+    @handgun_items << _item
+    addHandgunItem @this, _item
+  end
+
 end
