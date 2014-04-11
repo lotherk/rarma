1f766dadf7a1d2e563956c0fe71202b2e8ca4323
Lukas Kropatschek <lukas@kropatschek.net>
2014-04-11 17:03:23 +0200

    small cosmetic improvments for logger

--- sqf/rarma/lib/include/debug.h
+++ sqf/rarma/lib/include/debug.h
@@ -1,8 +1,7 @@
 #define LOGGER(message) [message, __LINE__, __FILE__, "info"] call Rarma_Logger_fnc_log;
 #define LOGGER_WITH_LEVEL(level, message) [message, __LINE__, __FILE__, level] call Rarma_Logger_fnc_log;
 #define LOGGER_INFO(message) LOGGER(message)
-#define LOGGER_DEBUG(message) LOGGER_WITH_LEVEL("debug", message)
-#define LOGGER_WARN(message) LOGGER_WITH_LEVEL("warn", message)
-#define LOGGER_ERROR(message) LOGGER_WITH_LEVEL("error", message)
-#define LOGGER_FATAL(message) LOGGER_WITH_LEVEL("fatal", message)
-
+#define LOGGER_DEBUG(message) LOGGER_WITH_LEVEL("DEBUG", message)
+#define LOGGER_WARN(message) LOGGER_WITH_LEVEL("WARN", message)
+#define LOGGER_ERROR(message) LOGGER_WITH_LEVEL("ERROR", message)
+#define LOGGER_FATAL(message) LOGGER_WITH_LEVEL("FATAL", message)
--- sqf/rarma/lib/logger.rb
+++ sqf/rarma/lib/logger.rb
@@ -1,10 +1,10 @@
 module Rarma::Logger
   __native
-  def self.log _message, _lineno, _file, _level="info"
+  def self.log _message, _lineno, _file, _level="INFO"
     <<-SQF
     // strip mission path from _file
 
-    _format = format["%1|%2|%3 %4:%5 %6", time, diag_frameno, diag_fps, _file, _lineno, _message];
+    _format = format["%1: %2|%3|%4 %5:%6 %7", _level, time, diag_frameno, diag_fps, _file, _lineno, _message];
     diag_log (text _format);
     SQF
   end
