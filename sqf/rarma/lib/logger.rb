38d9778e312bfdef7ba227ff2dd44c69e3b605a6
Konrad Lother <konrad@corpex.de>
2014-04-11 17:44:13 +0200

    added scriptName

--- sqf/rarma/lib/logger.rb
+++ sqf/rarma/lib/logger.rb
@@ -2,9 +2,15 @@ module Rarma::Logger
   __native
   def self.log _message, _lineno, _file, _level="INFO"
     <<-SQF
+    private "_scriptName";
     // strip mission path from _file
+    _scriptName = if (isnil "_fnc_scriptName") then {
+      "-"
+    } else {
+      _fnc_scriptName
+    };
 
-    _format = format["%1: %2|%3|%4 %5:%6 %7", _level, time, diag_frameno, diag_fps, _file, _lineno, _message];
+    _format = format["%1: %2|%3|%4 %5:%6 %7 %8", _level, time, diag_frameno, diag_fps, _file, _lineno, _scriptName, _message];
     diag_log (text _format);
     SQF
   end
