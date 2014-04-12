f0c586693ea2eafdd493a90f60e9445fd16d50c1
Konrad Lother <konrad@corpex.de>
2014-04-12 21:27:09 +0200

    ensure processor

---
+++ lib/rarma/sqf/compiler/processor/ensure.rb
@@ -0,0 +1,8 @@
+module Rarma::SQF::Compiler::Processor::Ensure
+  def process_ensure exp
+    resc = exp.shift
+    ens = exp.shift
+
+    exp
+  end
+end
--- lib/rarma/sqf/compiler/processor/rescue.rb
+++ lib/rarma/sqf/compiler/processor/rescue.rb
@@ -1,6 +1,12 @@
 module Rarma::SQF::Compiler::Processor::Rescue
   def process_rescue exp
-    raise exp.to_s
+    block = exp.shift
+    a = self.class.new
+    a.process block
+    block = a.script
+    while exp.count > 0
+      exp.shift
+    end
     exp
   end
 end
