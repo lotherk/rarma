e67cf5b6470c3ea765a2f6a13608c3701a871f27
Konrad Lother <konrad@corpex.de>
2014-04-13 13:08:09 +0200

    new preprocessor command __only_on (modules only!)

--- lib/rarma/sqf/compiler/processor/preprocessor.rb
+++ lib/rarma/sqf/compiler/processor/preprocessor.rb
@@ -11,4 +11,35 @@ module Rarma::SQF::Compiler::Processor::Preprocessor
     end
     exp
   end
+
+  def process_preprocessor_only_on exp
+    raise "__only_on works only on modules." unless @current_class.is_a?(Rarma::SQF::Compiler::Script::Module)
+
+    on = exp.shift[1]
+    opt = exp.shift
+    raise "__only_on: opts must be hash" unless opt.shift == :hash
+    opts = {}
+    while opt.count > 0
+      key = opt.shift[1]
+      val = opt.shift
+      if val.count > 1
+        val = val[1]
+      else
+        val = val[0]
+      end
+      opts[key] = val
+    end
+
+    raise "__only_on: unknown target: #{on}" unless [:client,:server,:dedicated,:headless].include?on
+    case on
+      when :server
+        @current_class.header << 'if(!isServer) exitWith {}'
+      when :dedicated
+        @current_class.header << 'if(!isDedicated) exitWith {}'
+      when :client
+        @current_class.header << 'if(!hasInterface || {isServer} || {isDedicated}) exitWith {}'
+      when :headless
+        @current_class.header << 'if(hasInterface || {isServer} || {isDedicated}) exitWith {}'
+    end
+  end
 end
--- lib/rarma/sqf/compiler/script/module.rb
+++ lib/rarma/sqf/compiler/script/module.rb
@@ -1,8 +1,10 @@
 class Rarma::SQF::Compiler::Script::Module
   attr_reader :meths, :name
+  attr_reader :header
   def initialize name=nil
     @meths = []
     @name = name
+    @header = []
   end
 
   def to_s
