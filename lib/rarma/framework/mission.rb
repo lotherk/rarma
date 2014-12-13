23eac5acf654d9ea09f207a41621bf439e0f41eb
Konrad Lother <konrad@corpex.de>
2014-12-13 12:32:24 +0100

    clean up

--- lib/rarma/framework/mission.rb
+++
@@ -1,8 +0,0 @@
-class Rarma::Framework::Mission
-  require "rarma/framework/mission/playable"
-  extend Rarma::Framework::Mission::Playable
-  
-  def self.script script
-    puts script
-  end
-end
