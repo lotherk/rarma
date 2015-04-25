b562785005ea97cf879756f55000e56db33c95ff
Konrad Lother <konrad@corpex.de>
2015-04-25 17:24:44 +0200

    basic new cli

--- lib/rarma/cli/subcommand/new.rb
+++ lib/rarma/cli/subcommand/new.rb
@@ -6,11 +6,22 @@ class Rarma::CLI::Subcommand::New
   end
   def main
     p = Trollop::Parser.new do
-      banner "Usage"
-      banner "Create a mission/addon from scratch"
+      banner <<EOB
+Usage
+  rarma [global options] new [options] name
+
+Create a mission/addon from scratch.
+EOB
+      banner ""
       opt :mission, 'create a new mission', :short => 'm'
       opt :addon, 'create a new addon', :short => 'a'
+      opt :description, 'short description', :short => 'D', :default => ''
+      opt :author, "author name", :default => ENV['USER'], :short => 'A'
+      opt :version, "initial version", :default => '0.1.0', :short => 'V'
+      opt :island, "specify island (only for missions)", :multi => true, :default => 'Altis', :short => 'I'
+      opt :name, "overwrite name", :short => 'N', :default => ''
       conflicts :mission, :addon
+      conflicts :island, :addon
     end
 
     @opts = Trollop::with_standard_exception_handling p do
