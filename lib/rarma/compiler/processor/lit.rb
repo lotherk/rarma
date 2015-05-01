fd3ab6e37493c5fdef12df7b8148987229d3c6fa
Konrad Lother <konrad@corpex.de>
2015-05-01 10:56:54 +0200

    was a nice idea at least...

--- lib/rarma/compiler/processor/lit.rb
+++ lib/rarma/compiler/processor/lit.rb
@@ -2,7 +2,7 @@ module Rarma::Compiler::Processor::Lit
   # process lit TODO
   def process_lit exp
     Rarma.logger.debug exp.to_s
-    @result << "([\"new\", #{exp.shift}] call RarmaLib_ProxyObject)"
+    @result << "([\"new\", #{exp.shift}] call RarmaLib_Number)"
     exp
   end
 end
