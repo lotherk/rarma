4c049e5e516e6e363fe886447914a78488f0fad5
Konrad Lother <konrad@corpex.de>
2015-05-02 14:53:27 +0200

    foo

--- lib/rarma/compiler/processor/gasgn.rb
+++ lib/rarma/compiler/processor/gasgn.rb
@@ -1,6 +1,6 @@
-module Rarma::Compiler::Processor::Lasgn
+module Rarma::Compiler::Processor::Gasgn
   # process left assignment
-  def process_lasgn exp
+  def process_gasgn exp
     Rarma.logger.debug exp.to_s
     left = exp.shift
     right = exp.shift
@@ -9,7 +9,7 @@ module Rarma::Compiler::Processor::Lasgn
     processor.scope = @scope
     processor.process right
 
-    @result << "%s = %s" % [@scope.get_private_viariable(left), processor.result.shift]
+    @result << "%s = %s" % [@scope.get_global_variable(left), processor.result.shift]
     exp
   end
 end
--- lib/rarma/compiler/scope.rb
+++ lib/rarma/compiler/scope.rb
@@ -1,20 +1,32 @@
 class Rarma::Compiler::Scope
-  attr_reader :scopes, :variables
+  attr_reader :scopes, :private_variables, :global_variables
   def initialize
     @scopes = []
-    @variables = {}
+    @private_variables = {}
   end
   def add scope
     @scopes << scope
   end
 
   def get_variable varname
-    if @variables.has_key?varname.to_sym
-      @variables[varname.to_sym]
+    get_private_variable varname
+  end
+
+  def get_private_variable varname
+    if @private_variables.has_key?varname.to_sym
+      @private_variables[varname.to_sym]
     else
       new_varname = "_#{varname}"
-      @variables[varname.to_sym] = new_varname
+      @private_variables[varname.to_sym] = new_varname
       new_varname # return modified
+    end
+  end
+
+  def get_global_variable varname
+    if @global_variables.has_key?varname.to_sym
+      @global_variables[varname.to_sym]
+    else
+      @global_variables[varname.to_sym] = varname
     end
   end
 end
